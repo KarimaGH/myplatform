012c46a112ee8e26befda3e9d4fba1f8aedfc040
Karima Ghali <karimaelghali@yahoo.fr>
2017-03-17 16:44:56 +0000

    qlq modif sur la page welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    My Platform
                 </div>
 
                 <div class="links">
